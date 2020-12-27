6ddda4c25f197352aa3475f4ee994db48348b239
Diego R Len <diegorodriguespro@outlook.com>
2020-12-26 22:55:22 -0300

    Adicionado cactus

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
 
 function jump (){
@@ -34,5 +35,16 @@ function handleKeyUp(event){
         }
     }
 }
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 0;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyUp);
